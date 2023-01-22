4b9e8bb88ca7186c4e3efffb2222be27d89d85ae
Alistair Chisholm <alistair@Alistairs-MacBook-Air.local>
2023-01-22 18:56:04 +0000

    config

--- tests/basket.spec.ts
+++ tests/basket.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('meta is correct', async ({ page }) => {
-    await page.goto("/basket");
+    await page.goto("http://localhost:3000/basket");
 
     await expect(page).toHaveTitle('Basket | TLC Electrical');
 });
